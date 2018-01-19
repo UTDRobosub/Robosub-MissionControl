44fcb656173d35ed82c78fc0a3907550f06de473
Ezra Anderson <ezradaleanderson@hotmail.com>
2018-01-19 10:43:16 -0600

    add threading

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <SDL/SDL.h>
-#include "controller.h"
 #include <iostream>
+#include <thread>
+#include "controller.h"
 using namespace std;
 
-void controller(){
+void control();
+
+int main(int argc, char* argv[]){
+
+    thread controlThread(control);
+
+    controlThread.join();
+
+    return 0;
+}
+
+//Thread for mission control window (everything but video feed)
+//Display and send controller inputs,
+//Recieve and display diagnostics
+void control(){
 
     //start SDL
     if(SDL_Init ( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK ) < 0 ){
@@ -11,54 +26,51 @@ void controller(){
         exit(1);
     }
 
-    //create controllers
-    Controller* controller1 = new Controller;
-    Controller* controller2 = new Controller;
-    controller1->setJoystick(SDL_JoystickOpen(0));
-    controller2->setJoystick(SDL_JoystickOpen(1));
-
-    //for event handling
-    SDL_Event e;
-    bool running = true;
+    //get controllers
+    Controller controller1;
+    Controller controller2;
+    controller1.setJoystick(SDL_JoystickOpen(0));
+    controller2.setJoystick(SDL_JoystickOpen(1));
 
     //make window, important locations in window
     SDL_Surface* window = SDL_SetVideoMode(771,1002,0,0);
     SDL_Rect* topScreen = new SDL_Rect; topScreen->x = 0; topScreen->y = 0;
     SDL_Rect* bottomScreen = new SDL_Rect; bottomScreen->x = 0; bottomScreen->y = 501;
 
+    SDL_Event event;
+    bool running = true;
+    //main loop
     while(running){
 
-        //event queue for key events
+        //event loop
         SDL_PumpEvents();
-        while( SDL_PollEvent( &e ) != 0 ) {
-            if( e.type == SDL_QUIT )
+        while( SDL_PollEvent( &event ) != 0 ) {
+            if( event.type == SDL_QUIT )
                 running = false;
-            else if( e.type == SDL_KEYDOWN ) {
-                if(e.key.keysym.sym == SDLK_ESCAPE)
+            else if( event.type == SDL_KEYDOWN ) {
+                if(event.key.keysym.sym == SDLK_ESCAPE)
                     running = false;
-                else if(e.key.keysym.sym == SDLK_r){
-                  SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
-                  SDL_InitSubSystem(SDL_INIT_JOYSTICK);
-                  controller1->setJoystick(SDL_JoystickOpen(0));
-                  controller2->setJoystick(SDL_JoystickOpen(1));
+                //if r is pressed, restart joystick subsystem
+                else if(event.key.keysym.sym == SDLK_r){
+                    SDL_QuitSubSystem(SDL_INIT_JOYSTICK);
+                    SDL_InitSubSystem(SDL_INIT_JOYSTICK);
+                    controller1.setJoystick(SDL_JoystickOpen(0));
+                    controller2.setJoystick(SDL_JoystickOpen(1));
                 }
             }
         }
 
-        //update and return joystick states
-        controller1->getStates();
-        controller2->getStates(); //package and send these
+
+        //update and get joystick states
+        controller1.getStates();
+        controller2.getStates(); //package and send these
 
         //update gui
-        SDL_BlitSurface(controller1->getScreen(),NULL,window,topScreen);
-        SDL_BlitSurface(controller2->getScreen(),NULL,window,bottomScreen);
+        SDL_BlitSurface(controller1.getScreen(),NULL,window,topScreen);
+        SDL_BlitSurface(controller2.getScreen(),NULL,window,bottomScreen);
         SDL_Flip(window);
     }
 
     SDL_Quit();
 }
 
-
-int main(int argc, char* argv[]){
-    controller();
-}
