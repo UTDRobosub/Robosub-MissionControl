9a0be58e23a4b777ccc4a3a034241ebbcf04777b
Ezra Anderson <ezradaleanderson@hotmail.com>
2018-03-19 20:35:32 -0500

    started client

--- src/server.cpp
+++ src/server.cpp
@@ -2,6 +2,8 @@
 
 using namespace robosub;
 using WsServer = robosub::ws::SocketServer<robosub::ws::WS>;
+using WsClient = robosub::ws::SocketClient<robosub::ws::WS>;
+
 
 #include <stdio.h>
 //#include <sys/ioctl.h> // For FIONREAD
@@ -90,6 +92,36 @@ void server() {
     // Start WS-server
     server.start();
   });
+
+  WsClient client("localhost:8081/echo");
+  client.on_message = [](shared_ptr<WsClient::Connection> connection, shared_ptr<WsClient::Message> message) {
+    cout << "Client: Message received: \"" << message->string() << "\"" << endl;
+
+    cout << "Client: Sending close connection" << endl;
+    connection->send_close(1000);
+  };
+
+  client.on_open = [](shared_ptr<WsClient::Connection> connection) {
+    cout << "Client: Opened connection" << endl;
+
+    string message = "Hello";
+    cout << "Client: Sending message: \"" << message << "\"" << endl;
+
+    auto send_stream = make_shared<WsClient::SendStream>();
+    *send_stream << message;
+    connection->send(send_stream);
+  };
+
+  client.on_close = [](shared_ptr<WsClient::Connection> /*connection*/, int status, const string & /*reason*/) {
+    cout << "Client: Closed connection with status code " << status << endl;
+  };
+
+  // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
+  client.on_error = [](shared_ptr<WsClient::Connection> /*connection*/, const robosub::ws::error_code &ec) {
+    cout << "Client: Error: " << ec << ", error message: " << ec.message() << endl;
+  };
+
+  client.start();
 
   //wait one second for server to start
   robosub::Time::waitMillis(1000);
@@ -237,6 +269,11 @@ void server() {
     }
 
     // cout << current << endl;
+
+    cout << client.get_connection() << endl;
+    auto ss = make_shared<WsClient::SendStream>();
+    *ss << current["index"];
+    client.get_connection()->send(ss);
 
     //send update to all active connections
     for(auto &connection : server.get_connections())
